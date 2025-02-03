3c082dde9b77e3b7f346bfe5955d5863149f1482
QuentinGeerts <quentin.geerts@bstorm.be>
2025-02-03 12:00:28 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // template: '<h1>Hello !</h1>',
   standalone: false,
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  // styles: "h1 {}",
 })
 export class AppComponent {
   title = 'TF_202502_BootcampNET__Angular';
